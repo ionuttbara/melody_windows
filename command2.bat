5bb8967124759b4ca088657a502c8afb364635a1
Ionut Bara <ionutbaraooo@gmail.com>
2024-06-15 14:41:03 +0300

    update

--- command2.bat
+++ command2.bat
@@ -12,9 +12,6 @@ wmic path Win32_PnPEntity where "name='High precision event timer'" call disable
 :: Unsplit Services
 for /f %%a in ('Reg query HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\InstallService\Stubification /v "EnableAppOffloading" /s ^| findstr "HKEY"') do (
 for /f %%i in ('Reg query "%%a" /v "EnableAppOffloading" ^| findstr "HKEY"') do (Reg add "%%i" /v "EnableAppOffloading" /t Reg_DWORD /d "0" /f) >nul)
-for /f "tokens=2 delims==" %%i in ('wmic os get TotalVisibleMemorySize /format:value') do set mem=%%i
-set /a ram=%mem% + 1024000
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control" /v "SvcHostSplitThresholdInKB" /t REG_DWORD /d "%ram%" /f
 :--------------------------------------
 
 :--------------------------------------
